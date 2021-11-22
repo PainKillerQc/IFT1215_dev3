fe08b455717b31f29cf1da980fb8ca36a277a9a9
PainKillerQc <lauzier-marc@hotmail.com>
2021-11-22 13:16:33 -0500

    ajout du code version haut niveau

---
+++ Devoir3.py
@@ -0,0 +1,16 @@
+tab = []
+
+for i in range(10):
+    tab.append(int(input("Valeur " + str(i) + ": ")))
+    
+valeurCherchee = int(input("Valeur cherchée : "))
+    
+index = -1
+  
+while (index + 10) >= 0:
+    if valeurCherchee == tab[index]:
+        break
+    else:
+        index -= 1
+    
+print(index)
